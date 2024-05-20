453cbfea44199d56dff1aa861274458d4586a0c6
hunterdyar <bloop@hdyar.com>
2024-05-20 17:10:23 -0400

    returns

--- scrub-lang/Parser/Parselets/ReturnParselet.cs
+++ scrub-lang/Parser/Parselets/ReturnParselet.cs
@@ -9,6 +9,10 @@ public class ReturnParselet : IPrefixParselet
 		//need to figure out how to handle return;
 		//Should the tokenizer start adding endExpressions in when we have line breaks? I think so probably.
 		
+		//todo... consider returns...
+		//I also want the rust syntax of whatever the last value in an expression being what that block evaluates to.
+		//If this is the case, then return is always a single keyword.
+		
 		if (parser.Peek(TokenType.EndExpression))
 		{
 			return new ReturnExpression(new NullExpression());
