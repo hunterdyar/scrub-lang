1e8e7f953eda9c5c988bab09725f7d8ade50aa49
hunterdyar <bloop@hdyar.com>
2024-05-23 08:19:07 -0400

    Expression block bug fix

--- scrub-lang/Parser/Parselets/AssignParselet.cs
+++ scrub-lang/Parser/Parselets/AssignParselet.cs
@@ -8,12 +8,14 @@ public class AssignParselet : IInfixParselet
 	public IExpression Parse(Parser parser, IExpression left, Token token)
 	{
 		var right = parser.ParseExpression(BindingPower.Assignment - 1);
-
-		if (!(left is IdentifierExpression))
-			throw new ParseException("The left-hand side of an assignment must be an identifier.");
-
-		var name = ((IdentifierExpression)left);
-		return new AssignExpression(name, right);
+		if (left is IdentifierExpression name)
+		{
+			return new AssignExpression(name, right);
+		}else if (left is IndexExpression index)
+		{
+			//todo: assign index expressions. A built-in called set that it replaces itself with?
+		}
+		throw new ParseException("The left-hand side of an assignment must be an identifier.");
 	}
 
 	public int GetBindingPower()
--- scrub-lang/Parser/Parselets/ExpressionGroupParselet.cs
+++ scrub-lang/Parser/Parselets/ExpressionGroupParselet.cs
@@ -4,18 +4,17 @@ namespace scrub_lang.Parser.Parselets;
 
 public class ExpressionGroupParselet : IPrefixParselet
 {
-	private List<IExpression> _expressions = new List<IExpression>();
 	public IExpression Parse(Parser parser, Token token)
 	{
-		_expressions.Clear();
+		List<IExpression> expressions = new List<IExpression>();
 		while (!parser.Peek(TokenType.EndExpressionBlock) && !parser.Peek(TokenType.EOF))
 		{
 			var e = parser.ParseExpression(0);
-			_expressions.Add(e);
+			expressions.Add(e);
 		}
 
 		parser.Consume(TokenType.EndExpressionBlock);
 
-		return new ExpressionGroupExpression(_expressions);
+		return new ExpressionGroupExpression(expressions);
 	}
 }
