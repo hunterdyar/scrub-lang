891c693c46fc5beff60d51bcef3ec68b925c0ef4
hunterdyar <bloop@hdyar.com>
2024-05-25 11:42:07 -0400

    test updates.

--- scrublangTests/VMTests.cs
+++ scrublangTests/VMTests.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.VisualBasic;
 using scrub_lang.Compiler;
 using scrub_lang.Objects;
+using Object = scrub_lang.Objects.Object;
 
 namespace scrub_lang.VirtualMachine;
 
-public static class VMTests
+public class VMTests
 {
 	public static List<VMTestCase> Tests = new List<VMTestCase>();
-
 	public static int Failures;
 	//VMTestCase struct
 	public struct VMTestCase
@@ -19,53 +19,14 @@ public static class VMTests
 		{
 			this.expected = expected;
 			this.input = input;
+			TestCase();
 		}
-	}
 
-	public static void RunAllVMTests()
-	{
-		TestIntegerArithmetic();
-		TestGlobalAssignStatements();
-	}
-	public static void TestIntegerArithmetic()
-	{
-		Console.WriteLine("Running VM Arthithmetic Tests...");
-		Failures = 0;
-		Tests = new List<VMTestCase>()
+		public bool TestCase()
 		{
-			new VMTestCase("1", new Integer(1)),
-			new VMTestCase("2", new Integer(2)),
-			new VMTestCase("1+2", new Integer(3)),
-			new VMTestCase("a = func(a){a+2}", VM.Null),// just writing this failing case down for now.
-			new VMTestCase("func a(){};a()", VM.Null)
-		};
-		RunTests();
-		Console.WriteLine($"Failures: {Failures}");
-
-	}
-
-	public static void TestGlobalAssignStatements()
-	{
-		Console.WriteLine("Running VM Global Assignment Tests...");
-		Failures = 0;
-		Tests = new List<VMTestCase>()
-		{
-			new VMTestCase("one = 1\n one", new Integer(1)),
-			new VMTestCase("one = 1\n two = 2\n one + two", new Integer(3)),
-			new VMTestCase("one = 1\ntwo = one+one\n one+two", new Integer(3))
-		};
-		RunTests();
-		Console.WriteLine($"Failures: {Failures}");
-
-	}
-
-	
-	public static void RunTests()
-	{
-		foreach (var test in Tests)
-		{
-			var p = VM.Parse(test.input);
+			var p = VM.Parse(input);
 			var comp = new Compiler.Compiler();
+
 			var error = comp.Compile(p);
 			if (error != null)
 			{
@@ -75,32 +36,64 @@ public static class VMTests
 			}
 
 			var vm = new VM(comp.ByteCode());
+
 			var vmerror = vm.Run();
 			if (vmerror != null)
 			{
 				Failures++;
 				throw new VMException(vmerror.ToString());
 			}
-			
-			//todo: compare the stacks
+
 			var top = vm.LastPopped();
-			CompareObjects(test.expected, top);
+			return CompareObjects(expected, top);
 		}
+	}
+	
+	[Test]
+	public void TestIntegerArithmetic()
+	{
+		new VMTestCase("1", new Integer(1));
+		new VMTestCase("2", new Integer(2));
+		new VMTestCase("1+2", new Integer(3));
+	
+	}
+
+	[Test]
+	public void TestGlobalAssignStatements()
+	{
+		new VMTestCase("one = 1", new Integer(1));
+		new VMTestCase("one = 1\n one", new Integer(1));
+		new VMTestCase("one = 1\n two = 2\n one + two", new Integer(3));
+		new VMTestCase("one = 1\ntwo = one+one\n one+two", new Integer(3));
+	}
+
+	[Test]
+	public void TestFunctionDec()
+	{
+		//todo: i am unable to compare closures (closures, compiled objects) correctly.
+		//new VMTestCase("func (){}", new Closure(new Function([(byte)OpCode.OpNull],0),null));
+		new VMTestCase("a = func(b){b+2};a(1)", new Integer(3));
+		new VMTestCase("func a(){};a()", VM.Null);
+		new VMTestCase("a = func b(){12};a()", new Integer(12));
+		new VMTestCase("b = func(a){a+1};c = b;c(2)", new Integer(3));
+		new VMTestCase("b = func(a){a()};c = b;c(func(){3})", new Integer(3));
+
 	}
 
 	static bool CompareObjects(object expected, object actual)
 	{
 		if (actual == null)
 		{
-			throw new VMException($"Test Failure. Expected {expected}, got null");
-			//todo: this won't catch if we want null... low priority.
+			Assert.Fail($"expected {expected}, got native null");
+			return false;
 		}
 
 		if (expected is Null n)
 		{
 			if (!(actual is Null))
 			{
-				throw new VMException($"test failure. Object not null: {actual}");
+				Assert.Fail($"Object not null: {actual}");
+				return false;
 			}//else
 			return true;
 		}
@@ -111,18 +104,33 @@ public static class VMTests
 				bool r = i.NativeInt == a.NativeInt;
 				if (!r)
 				{
-					Failures++;
-					throw new VMException($"Test Failure. e: {i} is not a: {a}.");
+					Assert.Fail($"expected: {i}, got {a}.");
+					return false;
 				}
 
 				return r;
 			}
-
-			Failures++;
-			throw new VMException($"Test Failure. e: {i} is not a: {actual.ToString()}.");
+			Assert.Fail($"e: {i} is not a: {actual.ToString()}.");
 		}
 
-		Failures++;
-		throw new VMException($"Test Failure. e: {expected} is not a: {actual.ToString()}.");
+		if (expected == actual)
+		{
+			return true;
+		}
+
+		var eo = (Object)expected;
+		var ao = (Object)actual;
+		//
+		if (eo.GetType() != ao.GetType())
+		{
+			Assert.Fail($" expected type {eo.GetType()}. Got {ao.GetType()}. e: {eo} a: {ao.ToString()}.");
+			return false;
+		}else if(eo.Bits.Xor(ao.Bits).OfType<bool>().All(e => !e)) //handle casts  )
+		{
+			Assert.Fail($" expected {eo.GetType()}. Expected {eo} is not a: {ao.ToString()}.");
+			return false;
+		}
+
+		return true;
 	}
 }
